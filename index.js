7cef01be00c19b93dac3aa502c2254e31c6a198b
David Borah <dborah87@gmail.com>
2018-08-02 19:02:52 -0400

    setup server skeleton

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+
+
+// setup our port
+const port = process.env.PORT || 8008;
+
+// power ups (middleware)
+
+
+// routes
+
+
+// kick it off
+server.listen(port, () => {
+    console.log(`Now listening on port: ${port}`);
+});
