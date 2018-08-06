5789e86200b639df5f6406d6925c1803705b689d
David Borah <dborah87@gmail.com>
2018-08-06 19:56:12 -0400

    Implemented our 404 handler middleware powerup

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ server.use(bodyParser.urlencoded({ extended: true }));  //accept html form data
 //routes
 server.use(petRouter);
 
+//404 handler
+server.use((req, res,) => {
+    res.status(404).json({
+        msg: "Resource not found"
+    });
+});
+
 // kick it off
 server.listen(port, () => {
     console.log(`Now listening on port: ${port}`);
